8ca72cfa9165479c977ccf8cae97001075e7c05f
Felipe256 <fcavalcante2013.fc@gmail.com>
2024-10-26 18:19:51 -0300

    Adicionando teste de preencher o formulario de login

--- features/step_definitions/login_step.rb
+++ features/step_definitions/login_step.rb
@@ -2,10 +2,32 @@ Dado /que eu estou na tela inicial/ do
   visit ''
 end
 
-Quando /eu pressiono "Entrar"/ do
-  find(:css, '#navbar > ul > li:nth-child(5) > a').click
+Dado /que eu estou no formulario de login/ do
+  steps %Q{
+    Dado que eu estou na tela inicial
+    Quando eu pressiono "Entrar" no cabeçalho 
+  }
+
 end
 
-Entao /eu deveria ver "(.*)" dentro do id "(.*)"/ do |texto, id|
-  find_by_id(id).assert_text(texto)
+Quando /eu pressiono "Entrar" no cabeçalho/ do
+  find('#navbar > ul > li:nth-child(5) > a').click
+end
+
+Quando /eu preencho "Nome de usuário" com "(.*)"/ do |nomeDeUsuario|
+  fill_in('Nome de usuário', :with => nomeDeUsuario)
+end
+
+Quando /eu preencho "Senha" com "(.*)"/ do |senha|
+  fill_in('Senha', :with => senha)
+end
+
+Quando /eu pressiono "Entrar" no formulario de login/ do
+  within("#modal-dialog") do
+    click_button('Entrar')
+  end
+end
+
+Entao /eu deveria ver "(.*)" dentro do css "(.*)"/ do |texto, css|
+  find(css).assert_text(texto)
 end
