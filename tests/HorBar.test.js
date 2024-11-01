231718ef491f6627d942bce719678d4f524371a8
Felipe Cavalcante Lins <fcavalcante2013.fc@gmail.com>
2024-10-31 22:02:35 -0300

    Adicionando teste do HorBar para que a tooltip nao seja mostrada ao carregar

--- tests/HorBar.test.js
+++ tests/HorBar.test.js
@@ -9,8 +9,22 @@ describe('HorBar.vue', () => {
         color: "purple"
       }
     });
-    console.log(wrapper.props)
     expect(wrapper.vm.style.toString()).toBe(({"background-image": "url(purple)", "width": "15%"}).toString())
   });
 
+  it('a tooltip nao deve ser mostrada ao carregar"', () => {
+    const wrapper = shallowMount(HorBar, {
+      propsData:{
+        percentage: 15,
+        color: "red",
+        'extraclass': "map",
+        absolute: ["R$", 500],
+        title: "Teste"
+      }
+    });
+    const classDivDesejada = "div.inline-block absolute z-10 py-2 px-3 text-sm font-medium text-white bg-gray-900 rounded-lg shadow-sm opacity-0 transition-opacity duration-300 tooltip"
+    const divToolTip = wrapper.find(classDivDesejada)
+    expect(divToolTip.exists()).toBe(false)
+  });
 });
+
