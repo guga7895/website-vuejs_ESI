c50fed1cb5c2dc7d3b699e0e8f5afea9a9420d35
Felipe Cavalcante Lins <fcavalcante2013.fc@gmail.com>
2024-10-31 21:00:01 -0300

    Corrigindo teste da funcao style do HorBar

--- tests/HorBar.test.js
+++ tests/HorBar.test.js
@@ -3,17 +3,14 @@ import HorBar from '@/components/chart/HorBar.vue'
 
 describe('HorBar.vue', () => {
   it('style deve devolver a cor e o numero correto"', () => {
-    const colorTest = "purple"
-    const numberTest = 6
     const wrapper = shallowMount(HorBar, {
       propsData:{
-        percentage: numberTest,
-        color: colorTest
+        percentage: 15,
+        color: "purple"
       }
     });
     console.log(wrapper.props)
-    const resultadoEsperado = '{background-image: url('+colorTest+'), '+numberTest+'%"}';
-    //expect(wrapper.vm.style).toBe(resultadoEsperado)
+    expect(wrapper.vm.style.toString()).toBe(({"background-image": "url(purple)", "width": "15%"}).toString())
   });
 
 });
